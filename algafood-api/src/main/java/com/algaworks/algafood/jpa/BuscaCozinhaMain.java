6d365e26d3e6cedddbf1b0d707d38fbad56e3128
danil <danil@192.168.0.106>
2022-11-09 17:32:05 -0300

    3.14. Conhecendo e implementando o padrão Repository

--- algafood-api/src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 public class BuscaCozinhaMain {
 
@@ -14,12 +15,12 @@ public class BuscaCozinhaMain {
 				.web(WebApplicationType.NONE)
 				.run(args);
 		
-		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		CozinhaRepository cozinhas = applicationContext.getBean(CozinhaRepository.class);
 		
-		Cozinha cozinha = cadastroCozinha.buscar(1L);
+		Cozinha TodasCozinhas = cozinhas.porId(1L);
 		
 		
-			System.out.println(cozinha.getNome());
+			System.out.println(TodasCozinhas.getNome());
 		
 	}
 	
