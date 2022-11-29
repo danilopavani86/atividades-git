7a6615984dc080e6f30f8c5e81e1dcb65a499e76
danil <danil@DESKTOP>
2022-11-29 14:48:19 -0300

    4.20. Manipulando a resposta HTTP com ResponseEntity

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,10 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,25 +17,36 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping(value = "/cozinhas")//, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 
-	@Autowired 
+	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
 	@GetMapping
-	public List<Cozinha> listar(){
-		return cozinhaRepository.listar();		
+	public List<Cozinha> listar() {
+		return cozinhaRepository.listar();
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml(){
-		return new CozinhasXmlWrapper(cozinhaRepository.listar());		
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+
+	@GetMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+//		return ResponseEntity.ok(cozinha);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.headers(headers)
+				.build();
 	}
 	
-	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable("cozinhaId") Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
-		
-	}
 }
