fe1ad11b559ca7bd310221fe9434c0f34ae73ca8
danil <danil@DESKTOP>
2022-12-08 16:26:22 -0300

    4.31. Desafio: Modelando e implementando a atualização de recursos de restaurantes

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -2,18 +2,21 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
@@ -58,5 +61,19 @@ public class RestauranteController {
 		}
 		
 	}
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<Restaurante> atualizar(@PathVariable Long restauranteId,
+			@RequestBody Restaurante restaurante) {
+		Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+		
+		if (restauranteAtual != null ) {
+		    BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+		
+			cadastroRestaurante.salvar(restauranteAtual);
+			return ResponseEntity.ok(restauranteAtual);
+		}
+		return ResponseEntity.notFound().build();
+	}	
+	
 	
 }
