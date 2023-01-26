205c49ba64f54bf7ec1a5119bf1974b3ccee19fe
danil <danil@DESKTOP>
2023-01-26 16:31:36 -0300

    6.1. Mapeando relacionamento bidirecional com @OneToMany

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,23 +1,41 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@JsonRootName("cozinha")
-@Data
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Entity
-public class Cozinha {
+	@Data
+	@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+	@Entity
+	public class Cozinha {
 
-	@EqualsAndHashCode.Include
+		@EqualsAndHashCode.Include
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@Column(nullable = false)
+		private String nome;
+		
+		@JsonIgnore
+		@OneToMany(mappedBy = "cozinha")
+		private List<Restaurante> restaurantes = new ArrayList<>();
+		
+	}
+	/*
+	 * @EqualsAndHashCode.Include
+	 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -27,3 +45,4 @@ public class Cozinha {
 	}
 
 	
+    private List<Restaurante> restaurantes = new ArrayList<>();*/
