74387b557ba987d118a3a998b3163e20817a26a2
danil <danil@DESKTOP>
2023-05-30 14:00:15 -0300

    9.16. Criando constraints de validação customizadas com implementação de ConstraintValidator

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -26,7 +26,8 @@ import javax.validation.groups.Default;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import com.algaworks.algafood.Groups;
+import com.algaworks.algafood.core.validation.Groups;
+import com.algaworks.algafood.core.validation.Multiplo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
@@ -46,12 +47,14 @@ public class Restaurante {
 	@Column(nullable = false)
 	private String nome;
 	
+	@NotNull
 	@PositiveOrZero
+	@Multiplo(numero = 5)
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
 	@Valid
-	@ConvertGroup(from = Default.class, to = Groups.CadastroRestaurante.class)
+	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
 	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
